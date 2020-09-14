49a607133a75759f77424548a8d1834a80e57c5b
agus tri adzie <agusmemons90@gmail.com>
2020-09-14 10:29:48 +0700

    remove swap on type change shift

--- app/library/notif_lib.js
+++ app/library/notif_lib.js
@@ -327,11 +327,11 @@ module.exports = async (data, callback) => {
                             end = {sup: 1, hr: 0, swap: 0, hr_approve: 'o', swap_approve: 'o', sup_approve: 'o'};
                             arr = {
                                 sup: value.supx,
-                                swap: value.swapx,
+                                swap: [],
                                 hr: [],
                                 supx_comp: value.supx_comp,
                                 hrx_comp: [],
-                                swapx_comp: value.swapx_comp
+                                swapx_comp: []
                             };
                         
                     }
